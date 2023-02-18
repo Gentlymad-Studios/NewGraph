6923d52e4f29e5ae0b0fde07e417668207b5f695
Matthias Guntrum <m.guntrum@gentlymadstudios.com>
2023-02-19 00:25:41 +0100

    next attempt at fixing scriptable object bug

--- Models/GraphModel.cs
+++ Models/GraphModel.cs
@@ -1,6 +1,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+using System;
+#endif
+
 namespace NewGraph {
     /// <summary>
     /// Runtime valid part of our GraphModel.
@@ -8,12 +13,149 @@ namespace NewGraph {
     /// See GraphModelEditorSpecifics.cs for all editor specific parts of this class.
     /// </summary>
     [CreateAssetMenu(fileName =nameof(GraphModel), menuName = "Tools/"+ nameof(GraphModel), order = 1)]
-    public partial class GraphModel : ScriptableObject {
+    public class GraphModel : ScriptableObject {
         /// <summary>
         /// List of all the nodes we want to work on.
         /// </summary>
         [SerializeReference]
         public List<NodeModel> nodes = new List<NodeModel>();
+#if UNITY_EDITOR
+        // FROM HERE BE DRAGONS...
+        [NonSerialized]
+        public SerializedObject serializedGraphData;
+        [NonSerialized]
+        private SerializedProperty nodesProperty = null;
+        [NonSerialized]
+        private SerializedProperty utilityNodesProperty = null;
+
+        [HideInInspector, SerializeField]
+        private string tmpNameProperty;
+
+        [SerializeField]
+        private bool viewportInitiallySet = false;
+        public bool ViewportInitiallySet => viewportInitiallySet;
+        private SerializedProperty viewportInitiallySetProperty = null;
+        private SerializedProperty ViewportInitiallySetProperty {
+            get {
+                if (viewportInitiallySetProperty == null) {
+                    viewportInitiallySetProperty = serializedGraphData.FindProperty(nameof(viewportInitiallySet));
+                }
+                return viewportInitiallySetProperty;
+            }
+        }
+
+        [SerializeField]
+        private Vector3 viewPosition;
+        public Vector3 ViewPosition => viewPosition;
+        [NonSerialized]
+        private SerializedProperty viewPositionProperty = null;
+        private SerializedProperty ViewPositionProperty {
+            get {
+                if (viewPositionProperty == null) {
+                    viewPositionProperty = serializedGraphData.FindProperty(nameof(viewPosition));
+                }
+                return viewPositionProperty;
+            }
+        }
+
+        [SerializeField]
+        private Vector3 viewScale;
+        public Vector3 ViewScale => viewScale;
+        [NonSerialized]
+        private SerializedProperty viewScaleProperty = null;
+        private SerializedProperty ViewScaleProperty {
+            get {
+                if (viewScaleProperty == null) {
+                    viewScaleProperty = serializedGraphData.FindProperty(nameof(viewScale));
+                }
+                return viewScaleProperty;
+            }
+        }
+
+
+        [SerializeReference]
+        public List<NodeModel> utilityNodes = new List<NodeModel>();
+
+        public void SetViewport(Vector3 position, Vector3 scale) {
+            if (position != viewPosition || scale != viewScale) {
+                ViewportInitiallySetProperty.boolValue = true;
+                ViewScaleProperty.vector3Value = scale;
+                ViewPositionProperty.vector3Value = position;
+                serializedGraphData.ApplyModifiedProperties();
+            }
+        }
+
+        public NodeModel AddNode(INode node, bool isUtilityNode) {
+            NodeModel baseNodeItem = new NodeModel(node);
+            baseNodeItem.isUtilityNode = isUtilityNode;
+            if (!isUtilityNode) {
+                nodes.Add(baseNodeItem);
+            } else {
+                utilityNodes.Add(baseNodeItem);
+            }
+            ForceSerializationUpdate();
+            return baseNodeItem;
+        }
+
+        public NodeModel AddNode(NodeModel nodeItem) {
+            if (!nodeItem.isUtilityNode) {
+                nodes.Add(nodeItem);
+            } else {
+                utilityNodes.Add(nodeItem);
+            }
+            return nodeItem;
+        }
+
+        public void RemoveNode(NodeModel node) {
+            if (!node.isUtilityNode) {
+                nodes.Remove(node);
+            } else {
+                utilityNodes.Remove(node);
+            }
+        }
+
+        public void RemoveNodes(List<NodeModel> nodesToRemove) {
+            foreach (NodeModel node in nodesToRemove) {
+                RemoveNode(node);
+            }
+        }
+
+        public void ForceSerializationUpdate() {
+            serializedGraphData.Update();
+            EditorUtility.SetDirty(this);
+            serializedGraphData.ApplyModifiedProperties();
+        }
+
+        public void CreateSerializedObject() {
+            serializedGraphData = new SerializedObject(this);
+            nodesProperty = null;
+            utilityNodesProperty = null;
+            viewPositionProperty = null;
+            viewScaleProperty = null;
+            viewportInitiallySetProperty = null;
+        }
+
+        public SerializedProperty GetNodesProperty(bool isUtilityNode) {
+            if (!isUtilityNode) {
+                if (nodesProperty == null) {
+                    nodesProperty = serializedGraphData.FindProperty(nameof(nodes));
+                }
+                return nodesProperty;
+            }
+
+            if (utilityNodesProperty == null) {
+                utilityNodesProperty = serializedGraphData.FindProperty(nameof(utilityNodes));
+            }
+            return utilityNodesProperty;
+        }
+
+        public SerializedProperty GetLastAddedNodeProperty(bool isUtilityNode) {
+            if (isUtilityNode) {
+                return GetNodesProperty(true).GetArrayElementAtIndex(utilityNodes.Count - 1);
+            }
+            return GetNodesProperty(false).GetArrayElementAtIndex(nodes.Count - 1);
+        }
+#endif
     }
 
 }
--- Models/NodeModel.cs
+++ Models/NodeModel.cs
@@ -1,6 +1,11 @@
 using System;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+using System.Collections.Generic;
+#endif
+
 namespace NewGraph {
     /// <summary>
     /// Runtime valid part of our NodeModel.
@@ -8,11 +13,106 @@ namespace NewGraph {
     /// See NodeModelEditorSpecifics.cs for all editor specific parts of this class.
     /// </summary>
     [Serializable]
-    public partial class NodeModel {
+    public class NodeModel {
         /// <summary>
         /// The actual node data.
         /// </summary>
         [SerializeReference]
         public INode nodeData;
+
+#if UNITY_EDITOR
+        // FROM HERE BE DRAGONS...
+        [SerializeField]
+        private float nodeX, nodeY;
+        [SerializeField]
+        private string name;
+        [NonSerialized]
+        private bool dataIsSet = false;
+        public bool isUtilityNode = false;
+        public const string nameIdentifier = nameof(name);
+        [NonSerialized]
+        private SerializedProperty serializedProperty;
+        [NonSerialized]
+        private SerializedProperty nodeDataSerializedProperty;
+        [NonSerialized]
+        private SerializedProperty nameProperty;
+        [NonSerialized]
+        private SerializedProperty nodeXProperty;
+        [NonSerialized]
+        private SerializedProperty nodeYProperty;
+        [NonSerialized]
+        private static Dictionary<Type, NodeAttribute> nodeInfo = new Dictionary<Type, NodeAttribute>();
+        [NonSerialized]
+        public Type nodeType;
+        [NonSerialized]
+        public NodeAttribute nodeAttribute;
+
+        public NodeModel(INode nodeData) {
+            this.nodeData = nodeData;
+            Initialize();
+        }
+
+        public Vector2 GetPosition() {
+            return new Vector2(nodeX, nodeY);
+        }
+
+        public string GetName() {
+            return name;
+        }
+
+        public static NodeAttribute GetNodeAttribute(Type type) {
+            if (!nodeInfo.ContainsKey(type)) {
+                nodeInfo.Add(type, Attribute.GetCustomAttribute(type, typeof(NodeAttribute)) as NodeAttribute);
+            }
+            return nodeInfo[type];
+        }
+
+        public void Initialize() {
+            nodeType = nodeData.GetType();
+            nodeAttribute = GetNodeAttribute(nodeType);
+        }
+
+        public string SetName(string name) {
+            this.name = name;
+            if (dataIsSet) {
+                nameProperty.serializedObject.Update();
+            }
+            return name;
+        }
+
+        public void SetPosition(float positionX, float positionY) {
+            if (dataIsSet) {
+                if (positionX != nodeX || positionY != nodeY) {
+                    nodeXProperty.floatValue = positionX;
+                    nodeYProperty.floatValue = positionY;
+                    nodeXProperty.serializedObject.ApplyModifiedProperties();
+                }
+            } else {
+                nodeX = positionX;
+                nodeY = positionY;
+            }
+        }
+
+        public SerializedProperty GetSerializedProperty() {
+            return serializedProperty;
+        }
+
+        public SerializedProperty GetSpecificSerializedProperty() {
+            return nodeDataSerializedProperty;
+        }
+
+        public SerializedProperty GetNameSerializedProperty() {
+            return nameProperty;
+        }
+
+        public void SetData(SerializedProperty serializedProperty) {
+            this.serializedProperty = serializedProperty;
+            nodeDataSerializedProperty = serializedProperty.FindPropertyRelative(nameof(nodeData));
+            nameProperty = serializedProperty.FindPropertyRelative(nameof(name));
+            nodeXProperty = serializedProperty.FindPropertyRelative(nameof(nodeX));
+            nodeYProperty = serializedProperty.FindPropertyRelative(nameof(nodeY));
+            dataIsSet = true;
+        }
+#endif
     }
 }
