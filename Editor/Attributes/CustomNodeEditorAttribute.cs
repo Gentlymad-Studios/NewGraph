61410ff7fa3734d931b066aab62b2a9c7ba56e74
Matthias Guntrum <m.guntrum@gentlymadstudios.com>
2023-03-04 02:11:02 +0100

    added documentaiton folder added more comments

--- Editor/Attributes/CustomNodeEditorAttribute.cs
+++ Editor/Attributes/CustomNodeEditorAttribute.cs
@@ -1,9 +1,19 @@
 using System;
 
 namespace NewGraph {
-
+    /// <summary>
+    /// This attribute needs to be attached to a class that derives from NodeEditor.
+    /// This is the glue needed to automatically inject/create the defined editor for a specific node type.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
     public class CustomNodeEditorAttribute : Attribute {
+        /// <summary>
+        /// This field is required and should contain the nodeType the node editor belongs to.
+        /// </summary>
         public Type nodeType;
+        /// <summary>
+        /// This is an optional field that defaults to false. It can be used to re-use the same NodeEditor on derived nodes.
+        /// </summary>
         public bool editorForChildClasses = false;
         public CustomNodeEditorAttribute(Type nodeType, bool editorForChildClasses=false) {
             this.nodeType = nodeType;
